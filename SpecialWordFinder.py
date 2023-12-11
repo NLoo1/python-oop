995b2fa2afe2c34100b6c90c292c44cfb993123b
Nicholas <NLoo1@outlook.com>
2023-12-11 17:53:09 -0500

    More work on SpecialWordFinder

--- SpecialWordFinder.py
+++ SpecialWordFinder.py
@@ -4,16 +4,17 @@ class SpecialWordFinder(WordFinder):
 
     def __init__(self, file_path):
         super().__init__(file_path)
+        self.word_list = self.read_file()
 
     def __repr__(self):
         return f"SpecialWordFinder {self.file_path}"
     
     def read_file(self):
-        super().read_file()
-        for line in self.word_list:
-            if(line[0] == "#" or line.isspace()):
-                self.word_list.pop(self.word_list.index(line))
-        return self.word_list
+        unfiltered_list = super().read_file()
+        for line in unfiltered_list:
+            if("#" in line or line.isspace()):
+                unfiltered_list.remove(line)
+        return unfiltered_list
 
     def random(self):
         super().random()
