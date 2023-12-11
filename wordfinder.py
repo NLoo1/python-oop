3c04f62db04f23acdf703b5f04d323695fcf3300
Nicholas L <NLoo1@outlook.com>
2023-12-11 15:53:49 -0500

    Added WordFinder

--- wordfinder.py
+++ wordfinder.py
@@ -1,5 +1,26 @@
 """Word Finder: finds random words from a dictionary."""
 
+from random import randint
 
 class WordFinder:
     ...
+    def __init__(self, file_path):
+        self.file_path = file_path
+        self.word_list = self.read_file()
+
+    def __repr__(self):
+        return f"WordFinder: {self.file_path}"
+    
+    def read_file(self):
+        word_list = []
+        file = open(self.file_path)
+        for line in file:
+            new_word = line.split("\n")[0]
+            word_list.append(new_word)
+        file.close()
+        return word_list
+
+    def random(self):
+        random_int = randint(0, len(self.word_list))
+        return self.word_list[random_int]
+
