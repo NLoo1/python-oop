17da05ef49e8346873cbf53b2a4878aa6ad0d421
Nicholas L <NLoo1@outlook.com>
2023-12-11 01:51:34 -0500

    Finished serial

--- serial.py
+++ serial.py
@@ -20,3 +20,19 @@ class SerialGenerator:
     100
     """
 
+    def __init__(self, start=0):
+        self.start = start-1
+
+    def generate(self):
+        """Increases start by 1 and returns new start number"""
+        self.start+=1
+        return self.start
+    
+    def __repr__(self):
+        """ Returns repr of SerialGenerator"""
+        return f"SerialGenerator {self.start}"
+
+    def reset(self):
+        """Sets serial number back to 99. Serial number is set to 99 to account for generate() adding +1"""
+        self.start = 99
+
